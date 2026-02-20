5a4e8663520a6ac19e14e1faae28c12a17d02909
Tatiana Tochaeva <t.tochaeva@yandex.ru>
2026-02-20 17:10:37 +0300

    Second task. Adding the output of the last commit

--- 20260216/1/prog.py
+++ 20260216/1/prog.py
@@ -4,7 +4,17 @@ import zlib
 
 while True:
     data_str = input().split()
-    if len(data_str) == 1:
+    if data_str == ['end']:
+        break
+    elif len(data_str) == 1:
         data_gen = Path(data_str[0]).glob('.git/refs/heads/*')
         print(*[str(data).split('/')[-1] for data in data_gen], sep='\n')
+
+    elif len(data_str) == 3:
+        branch = Path(data_str[1] + '.git/refs/heads/' + data_str[2])
+        if data_str[0] == 'commit':
+            data_hesh = str(branch.read_bytes().decode('utf-8'))
+            obj_commit = Path(data_str[1] + '.git/objects/' + data_hesh[:2] + '/'+ data_hesh[2:-1])
+            data = zlib.decompress(obj_commit.read_bytes()).partition(b'\x00')
+            print(data[-1].decode('utf-8'))
 
