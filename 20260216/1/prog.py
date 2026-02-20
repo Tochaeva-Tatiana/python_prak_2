9cb1fbff469dcbad23363192427a55d91ff9871a
Tatiana Tochaeva <t.tochaeva@yandex.ru>
2026-02-20 13:49:54 +0300

    Change reading repo

--- 20260216/1/prog.py
+++ 20260216/1/prog.py
@@ -5,5 +5,6 @@ import zlib
 while True:
     data_str = input().split()
     if len(data_str) == 1:
-        data_gen = Path(data_str[0]).iterdir()
-        print(*[str(data).split('/')[-1] for data in data_gen])
+        data_gen = Path(data_str[0]).glob('.git/refs/heads/*')
+        print(*[str(data).split('/')[-1] for data in data_gen], sep='\n')
+
