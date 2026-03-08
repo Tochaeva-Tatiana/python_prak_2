774c9ff22ad9d45d5d6b640f803904a88a8b7779
Tatiana Tochaeva <t.tochaeva@yandex.ru>
2026-03-08 20:55:46 +0300

    Adding custom monster

--- 20260302/1/prog.py
+++ 20260302/1/prog.py
@@ -1,5 +1,22 @@
-from cowsay import cowsay, list_cows
+from cowsay import cowsay, list_cows, read_dot_cow
 import sys
+from io import StringIO
+
+jgsbat = read_dot_cow(StringIO(r"""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\\\--//|.'-._  (
+     )'   .'\/o\/o\/'.   `(
+      ) .' . \====/ . '. (
+       )  / <<    >> \  (
+        '-._/``  ``\_.-'
+  jgs     __\\\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
 
 field = [["-" for j in range(10)] for i in range(10)]
 position_x, position_y = 0, 0
@@ -16,7 +33,13 @@ if len(sys.argv) > 1:
 def encounter(x: int, y: int):
     if field[y][x] != '-':
         hello, name = field[y][x][0], field[y][x][1]
-        print(cowsay(hello, cow=name))
+        if name == 'jgsbat':
+        # Используем cowfile
+            print(cowsay(hello, cowfile=jgsbat))
+        else:
+        # Используем имя (для стандартных коров)
+            print(cowsay(hello, cow=name))
+
 
 print("<<< Welcome to Python-MUD 0.1 >>>")
 while True:
@@ -29,16 +52,18 @@ while True:
         
         if (len(args) == 4):
             name, x, y, hello = args[0], args[1], args[2], args[3]
-            if (x  in list('1234567890')) and (y in list('1234567890')) and (name in list_cows()):
+            if (x  in list('1234567890')) and (y in list('1234567890')) and (name in (list_cows() + ['jgsbat'])):
                 x = int(x)
                 y = int(y)
                 field[y][x] = [hello, name]
                 print(f"Added monster {name} to ({x}, {y}) saying {hello}")
             else:
                 print("Invalid arguments")
+                print(1)
 
         else:
             print("Invalid arguments")
+            print(2)
     elif com in ["up", "down", "left", "right"]:
         if len(args) == 0:
             if com == "up":
