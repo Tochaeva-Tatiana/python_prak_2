b825eec6601f1ceff6d52890a213316c33c8e210
Tatiana Tochaeva <t.tochaeva@yandex.ru>
2026-03-29 16:55:58 +0300

    Copy last homework

---
+++ 20260312/1/prog.py
@@ -0,0 +1,95 @@
+from cowsay import cowsay, list_cows, read_dot_cow
+import sys
+from io import StringIO
+
+jgsbat = read_dot_cow(StringIO(r"""
+$the_cow = <<EOC;
+         $thoughts
+          $thoughts
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\\\--//|.'-._  (
+     )'   .'\/o\/o\/'.   `(
+      ) .' . \====/ . '. (
+       )  / <<    >> \  (
+        '-._/``  ``\_.-'
+  jgs     __\\\\'--'//__
+         (((""`  `"")))
+EOC
+"""))
+
+import shlex
+
+field = [["-" for j in range(10)] for i in range(10)]
+position_x, position_y = 0, 0
+
+fl = False
+if len(sys.argv) > 1:
+    with open(sys.argv[1], 'r') as f:
+        lst = f.readlines()
+        fl = True
+        i = 0
+
+
+
+def encounter(x: int, y: int):
+    if field[y][x] != '-':
+        hello, name = field[y][x][0], field[y][x][1]
+        if name == 'jgsbat':
+        # Используем cowfile
+            print(cowsay(hello, cowfile=jgsbat))
+        else:
+        # Используем имя (для стандартных коров)
+            print(cowsay(hello, cow=name))
+
+
+print("<<< Welcome to Python-MUD 0.1 >>>")
+while True:
+    if fl:
+        com, *args = shlex.split(lst[i])
+        i += 1
+    else:
+        com, *args = shlex.split(input())
+    
+    if com == "addmon":
+        if (len(args) == 8):
+            name = args[0]
+            j = 1
+            while j < len(args):
+                if args[j] == 'hp':
+                    hp = args[j+1]
+                elif args[j] == 'hello':
+                    hello = args[j+1]
+                elif args[j] == 'coords':
+                    x, y = args[j+1], args[j+2]
+                    j += 1
+                j += 2
+            if (hp.isdigit()) and (x  in list('1234567890')) and (y in list('1234567890')) and (name in (list_cows() + ['jgsbat'])):
+                x = int(x)
+                y = int(y)
+                hp = int(hp)
+                field[y][x] = [hello, name, hp]
+                print(f"Added monster {name} to ({x}, {y}) saying {hello}")
+            else:
+                print("Invalid arguments")
+                print(1)
+
+        else:
+            print("Invalid arguments")
+            print(2)
+    elif com in ["up", "down", "left", "right"]:
+        if len(args) == 0:
+            if com == "up":
+                position_y = (position_y + 1) % 10
+            elif com == "down":
+                position_y = (position_y - 1) % 10
+            elif com == "left":
+                position_x = (position_x - 1) % 10
+            else:
+                position_x = (position_x + 1) % 10
+            print(f"Moved to ({position_x}, {position_y})")
+            encounter(position_x, position_y)
+        else:
+            print("Invalid arguments")
+    else:
+        print("Invalid command")
