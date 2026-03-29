5341f7398122a068ffa025292f5fc0b5d3ab7c85
Tatiana Tochaeva <t.tochaeva@yandex.ru>
2026-03-29 18:25:25 +0300

    Add attack + read file + fix

--- 20260312/1/prog.py
+++ 20260312/1/prog.py
@@ -9,7 +9,8 @@ class cmd_cow(cmd.Cmd):
 
     field = [["-" for j in range(10)] for i in range(10)]
     position_x, position_y = 0, 0
-    file = None
+    file_com = None
+    i = 0
 
     jgsbat = read_dot_cow(StringIO(r"""
     $the_cow = <<EOC;
@@ -31,7 +32,7 @@ class cmd_cow(cmd.Cmd):
         super().__init__()
         if len(sys.argv) > 1:
             with open(sys.argv[1], 'r') as f:
-                self.file = f.readlines()
+                self.file_com = [shlex.split(line.strip()) for line in f if line.strip()]
 
     def encounter(self, x: int, y: int):
         if self.field[y][x] != '-':
@@ -55,7 +56,8 @@ class cmd_cow(cmd.Cmd):
                     x, y = data[j+1], data[j+2]
                     j += 1
                 j += 2
-            if (hp.isdigit()) and (x.isdigit()) and (y.isdigit()) and (name in (list_cows() + ['jgsbat'])):
+            if (hp.isdigit()) and (x in list("1234567890")) and (y in list("1234567890")) \
+                and (name in (list_cows() + ['jgsbat'])):
                 x = int(x)
                 y = int(y)
                 hp = int(hp)
@@ -63,84 +65,82 @@ class cmd_cow(cmd.Cmd):
                 print(f"Added monster {name} to ({x}, {y}) saying {hello}")
             else:
                 print("Invalid arguments")
+        else:
+            print("Invalid command")
 
 
     def do_up(self, args):
         if not args:
+            
             self.position_y = (self.position_y + 1) % 10
             self.encounter(self.position_x, self.position_y)
+            print(f"Moved to ({self.position_x}, {self.position_y})")
+        else:
+            print("Invalid arguments")
   
 
     def do_down(self, args):
         if not args:
             self.position_y = (self.position_y - 1) % 10
             self.encounter(self.position_x, self.position_y)
+            print(f"Moved to ({self.position_x}, {self.position_y})")
+        else:
+            print("Invalid arguments")
 
 
     def do_left(self, args):
         if not args:
             self.position_x = (self.position_x - 1) % 10
             self.encounter(self.position_x, self.position_y)
+            print(f"Moved to ({self.position_x}, {self.position_y})")
+        else:
+            print("Invalid arguments")
                 
 
     def do_right(self, args):
         if not args:
             self.position_x = (self.position_x + 1) % 10
             self.encounter(self.position_x, self.position_y)
+            print(f"Moved to ({self.position_x}, {self.position_y})")
+        else:
+            print("Invalid arguments")
+
+    def do_attack(self, args):
+        if not args:
+            if self.field[self.position_y][self.position_x] == '-':
+                print("No monster here")
+            else:
+                monster = self.field[self.position_y][self.position_x]
+                attack = min(10, monster[2])
+                monster[2] -= attack
+                print(f"Attacked {monster[1]},  damage {attack} hp")
+                if monster[2] == 0:
+                    print(f"{monster[1]} died")
+                    self.field[self.position_y][self.position_x] = '-'
+                else:
+                    print(f"{monster[1]} now has {monster[2]}")
+        else:
+            print("Invalid arguments")
+
+
+    def cmdloop(self):
+        print("<<< Welcome to Python-MUD 0.1 >>>")
+        if self.file_com:
+            while self.i < len(self.file_com):
+                command_parts = self.file_com[self.i]
+                self.i += 1
+                command_str = " ".join(command_parts)
+                stop = self.onecmd(command_str)
+                if stop:
+                    return
+        else:
+            super().cmdloop()
+
+    def do_EOF(self, args):
+        return True
 
 
 
 if __name__ == '__main__':
     mud_game = cmd_cow()
     mud_game.cmdloop()
-
-
-# print("<<< Welcome to Python-MUD 0.1 >>>")
-# while True:
-#     if fl:
-#         com, *args = shlex.split(lst[i])
-#         i += 1
-#     else:
-#         com, *args = shlex.split(input())
-    
-#     if com == "addmon":
-#         if (len(args) == 8):
-#             name = args[0]
-#             j = 1
-#             while j < len(args):
-#                 if args[j] == 'hp':
-#                     hp = args[j+1]
-#                 elif args[j] == 'hello':
-#                     hello = args[j+1]
-#                 elif args[j] == 'coords':
-#                     x, y = args[j+1], args[j+2]
-#                     j += 1
-#                 j += 2
-#             if (hp.isdigit()) and (x  in list('1234567890')) and (y in list('1234567890')) and (name in (list_cows() + ['jgsbat'])):
-#                 x = int(x)
-#                 y = int(y)
-#                 hp = int(hp)
-#                 field[y][x] = [hello, name, hp]
-#                 print(f"Added monster {name} to ({x}, {y}) saying {hello}")
-#             else:
-#                 print("Invalid arguments")
-
-#         else:
-#             print("Invalid arguments")
-#     elif com in ["up", "down", "left", "right"]:
-#         if len(args) == 0:
-#             if com == "up":
-#                 position_y = (position_y + 1) % 10
-#             elif com == "down":
-#                 position_y = (position_y - 1) % 10
-#             elif com == "left":
-#                 position_x = (position_x - 1) % 10
-#             else:
-#                 position_x = (position_x + 1) % 10
-#             print(f"Moved to ({position_x}, {position_y})")
-#             encounter(position_x, position_y)
-#         else:
-#             print("Invalid arguments")
-#     else:
-#         print("Invalid command")
-
