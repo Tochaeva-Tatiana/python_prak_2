17f5cb38275621182780fc4c2f45c8e4f43a6df9
Точаева Татьяна <t.tochaeva@yandex.ru>
2026-05-09 16:05:23 +0300

    Add multiplayer MUD server and client

--- 20260323/1/client.py
+++ 20260323/1/client.py
@@ -2,47 +2,52 @@ from cowsay import list_cows
 import cmd
 import shlex
 import socket
+import asyncio
+import sys
 
 
 class CmdCow(cmd.Cmd):
 
+    prompt = "(MUD) "
+
+    def __init__(self, username, reader, writer):
+        super().__init__()
+        self.username = username
+        self.reader = reader
+        self.writer = writer
+
     def request(self, command):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(("localhost", 1337))
-        sock.sendall(command.encode())
-        answer = sock.recv(4096).decode()
-        sock.close()
-        return answer
+        self.writer.write((command + "\n").encode())
 
     def do_addmon(self, args):
-        print(self.request("addmon " + args))
+        self.request("addmon " + args)
 
     def do_up(self, args):
         if not args:
-            print(self.request("up"))
+            self.request("up")
         else:
             print("Invalid arguments")
 
     def do_down(self, args):
         if not args:
-            print(self.request("down"))
+            self.request("down")
         else:
             print("Invalid arguments")
 
     def do_left(self, args):
         if not args:
-            print(self.request("left"))
+            self.request("left")
         else:
             print("Invalid arguments")
 
     def do_right(self, args):
         if not args:
-            print(self.request("right"))
+            self.request("right")
         else:
             print("Invalid arguments")
 
     def do_attack(self, args):
-        print(self.request("attack " + args))
+        self.request("attack " + args)
 
     def complete_attack(self, text, line, begidx, endidx):
         data = shlex.split(line)
@@ -61,14 +66,73 @@ class CmdCow(cmd.Cmd):
             return [w for w in weapons if w.startswith(text)]
         return []
 
-    def cmdloop(self):
-        print("<<< Welcome to Python-MUD 0.1 >>>")
-        super().cmdloop()
+    def do_quit(self, args):
+        self.request("quit")
+        return True
 
     def do_EOF(self, args):
-        return True
+        print()
+        return self.do_quit(args)
+
+
+async def read_server(reader):
+    while True:
+        answer = await reader.readline()
+
+        if not answer:
+            print("\nDisconnected from server")
+            break
+
+        print(answer.decode().rstrip(), flush=True)
+
+async def read_user(cmdline):
+    loop = asyncio.get_event_loop()
+
+    while True:
+        line = await loop.run_in_executor(None, input)
+
+        if line == "EOF":
+            line = "quit"
+
+        stop = cmdline.onecmd(line)
+
+        await cmdline.writer.drain()
+
+        if stop:
+            cmdline.writer.close()
+            await cmdline.writer.wait_closed()
+            break
+
+
+async def main():
+    if len(sys.argv) != 2:
+        print("Usage: python client.py username")
+        return
+
+    username = sys.argv[1]
+
+    reader, writer = await asyncio.open_connection("localhost", 1337)
+
+    writer.write((username + "\n").encode())
+    await writer.drain()
+
+    answer = (await reader.readline()).decode().strip()
+
+    if answer != "OK":
+        print(answer)
+        writer.close()
+        await writer.wait_closed()
+        return
+
+    print("<<< Welcome to Python-MUD 0.1 >>>")
+
+    cmdline = CmdCow(username, reader, writer)
+
+    await asyncio.gather(
+        read_server(reader),
+        read_user(cmdline)
+    )
 
 
 if __name__ == "__main__":
-    mud_game = CmdCow()
-    mud_game.cmdloop()
+    asyncio.run(main())
--- 20260323/1/server.py
+++ 20260323/1/server.py
@@ -2,30 +2,37 @@ from cowsay import cowsay, list_cows, read_dot_cow
 from io import StringIO
 import shlex
 import socket
+import asyncio
 
 
 class Game:
 
     def __init__(self):
         self.field = [["-" for j in range(10)] for i in range(10)]
-        self.position_x = 0
-        self.position_y = 0
+        self.players = {}
+
 
         self.jgsbat = read_dot_cow(StringIO(r"""
         $the_cow = <<EOC;
                 $thoughts
                 $thoughts
-            ,_                    _,
-            ) '-._  ,_    _,  _.-' (
+             ,_                    _,
+             ) '-._  ,_    _,  _.-' (
             )  _.-'.|\\\\--//|.'-._  (
-            )'   .'\/o\/o\/'.   `(
-            ) .' . \====/ . '. (
-            )  / <<    >> \  (
+              )'   .'\/o\/o\/'.   `(
+               ) .' . \====/ . '. (
+                )  / <<    >> \  (
                 '-._/``  ``\_.-'
-        jgs     __\\\\'--'//__
-                (((""`  `"")))
+        jgs      __\\\\'--'//__
+                 (((""`  `"")))
         EOC
         """))
+    
+    def add_player(self, username):
+        self.players[username] = [0, 0]
+
+    def del_player(self, username):
+        self.players.pop(username, None)
 
     def encounter(self, x, y):
         if self.field[y][x] != "-":
@@ -35,7 +42,7 @@ class Game:
             return cowsay(hello, cow=name)
         return ""
 
-    def addmon(self, args):
+    def addmon(self, args, username):
         data = shlex.split(args)
         if len(data) == 8:
             name = data[0]
@@ -65,94 +72,163 @@ class Game:
                 y = int(y)
                 hp = int(hp)
                 self.field[y][x] = [hello, name, hp]
-                return f"Added monster {name} to ({x}, {y}) saying {hello}"
-            return "Invalid arguments"
-        return "Invalid command"
+                return "", [f"{username} added monster {name} to ({x}, {y}) with {hp} hp"]
+            return "Invalid arguments", []
+        return "Invalid command", []
 
-    def move(self, direction):
+    def move(self, direction, username):
+        position_x, position_y = self.players[username]
         if direction == "up":
-            self.position_y = (self.position_y + 1) % 10
+            position_y = (position_y + 1) % 10
         elif direction == "down":
-            self.position_y = (self.position_y - 1) % 10
+            position_y = (position_y - 1) % 10
         elif direction == "left":
-            self.position_x = (self.position_x - 1) % 10
+            position_x = (position_x - 1) % 10
         elif direction == "right":
-            self.position_x = (self.position_x + 1) % 10
+            position_x = (position_x + 1) % 10
+        
+        self.players[username] = [position_x, position_y]
 
-        answer = self.encounter(self.position_x, self.position_y)
+        answer = self.encounter(position_x, position_y)
         if answer:
             answer += "\n"
-        answer += f"Moved to ({self.position_x}, {self.position_y})"
-        return answer
+        answer += f"Moved to ({position_x}, {position_y})"
+        return answer, []
 
-    def attack(self, args):
+    def attack(self, args, username):
         data = shlex.split(args)
         if len(data) == 1 or len(data) == 3:
-            monster = self.field[self.position_y][self.position_x]
+            position_x, position_y = self.players[username]
+            monster = self.field[position_y][position_x]
 
             if monster == "-":
-                return "No monster here"
+                return "No monster here", []
             if monster[1] != data[0]:
-                return f"No {data[0]} here"
+                return f"No {data[0]} here", []
 
             if len(data) == 1:
                 damage = 10
+                weapon = "sword" 
             else:
                 if data[1] != "with":
-                    return "Invalid arguments"
-                if data[2] == "sword":
+                    return "Invalid arguments", []
+                weapon = data[2]
+                if weapon == "sword":
                     damage = 10
-                elif data[2] == "spear":
+                elif weapon == "spear":
                     damage = 15
-                elif data[2] == "axe":
+                elif weapon == "axe":
                     damage = 20
                 else:
-                    return "Unknown weapon"
+                    return "Unknown weapon", []
 
             attack = min(damage, monster[2])
             monster[2] -= attack
-            answer = f"Attacked {monster[1]},  damage {attack} hp"
+            answer =  f"{username} attacked {monster[1]} with {weapon}, damage {attack} hp, {monster[2]} hp left"
 
             if monster[2] == 0:
                 answer += f"\n{monster[1]} died"
-                self.field[self.position_y][self.position_x] = "-"
-            else:
-                answer += f"\n{monster[1]} now has {monster[2]}"
+                self.field[position_y][position_x] = "-"
 
-            return answer
-        return "Invalid arguments"
+            return [], [answer]
+        return "Invalid arguments", []
 
-    def process(self, command):
+    def process(self, command, username):
         data = shlex.split(command)
         if not data:
-            return ""
+            return "", []
 
         if data[0] == "addmon":
-            return self.addmon(command[len("addmon "):])
+            return self.addmon(command[len("addmon "):], username)
         if data[0] in ["up", "down", "left", "right"]:
             if len(data) == 1:
-                return self.move(data[0])
-            return "Invalid arguments"
+                return self.move(data[0], username)
+            return "Invalid arguments", []
         if data[0] == "attack":
-            return self.attack(command[len("attack "):])
+            return self.attack(command[len("attack "):], username)
 
-        return "Invalid command"
+        return "Invalid command", []
 
 
-def main():
-    game = Game()
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("localhost", 1337))
-    sock.listen(1)
+class Server:
 
-    while True:
-        conn, addr = sock.accept()
-        with conn:
-            command = conn.recv(4096).decode()
-            answer = game.process(command)
-            conn.sendall(answer.encode())
+    def __init__(self):
+        self.game = Game()
+        self.clients = {}
+
+    async def send_to(self, username, message):
+        if not message:
+            return
+
+        writer = self.clients.get(username)
+        if writer is None:
+            return
+
+        writer.write((message + "\n").encode())
+        await writer.drain()
+
+    async def broadcast(self, message):
+        if not message:
+            return
+
+        for username in list(self.clients):
+            await self.send_to(username, message)
+
+    async def handle_client(self, reader, writer):
+        username = ""
+
+        try:
+            username = (await reader.readline()).decode().strip()
+
+            if not username or " " in username or username in self.clients:
+                writer.write(b"ERROR bad or busy username\n")
+                await writer.drain()
+                return
+
+            self.clients[username] = writer
+            self.game.add_player(username)
+
+            writer.write(b"OK\n")
+            await writer.drain()
+
+            await self.broadcast(f"{username} entered MUD")
+
+            while not reader.at_eof():
+                command = (await reader.readline()).decode().strip()
+
+                if not command:
+                    continue
+
+                if command == "quit":
+                    break
+
+                answer, messages = self.game.process(command, username)
+
+                await self.send_to(username, answer)
+
+                for message in messages:
+                    await self.broadcast(message)
+
+        finally:
+            if username in self.clients:
+                self.clients.pop(username)
+                self.game.del_player(username)
+                await self.broadcast(f"{username} left MUD")
+
+            writer.close()
+            await writer.wait_closed()
+
+    async def run(self):
+        server = await asyncio.start_server(
+            self.handle_client,
+            "localhost",
+            1337
+        )
+
+        async with server:
+            await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(Server().run())
